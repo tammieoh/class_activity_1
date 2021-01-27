c4f50aaaf48e3214ddd65bbfb43f613661b6f9ba
tammieoh <oht@oxy.edu>
2021-01-27 00:43:53 -0800

    first commit

---
+++ app/src/main/java/com/example/classactivity1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.classactivity1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class1);
+    }
+}
